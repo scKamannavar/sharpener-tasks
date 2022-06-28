8260f8d51d1c7375dbfe901b0eb6e24f1b720f63
scKamannavar <sagar080897@gmail.com>
2022-06-28 19:50:01 +1300

    Header border changed

--- task3/dom.js
+++ task3/dom.js
@@ -1,4 +1,8 @@
-console.dir(document.title);
-document.title = 'Magnum Page';
-// console.log(Document);
-console.log(document.body);
+// console.dir(document.title);
+// document.title = 'Magnum Page';
+// // console.log(Document);
+// console.log(document.body);
+
+let x = document.getElementById('main-header');
+console.log(x);
+x.style.borderBottom = 'solid 2px black';
