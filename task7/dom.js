8bef5f06e7d0b7d2682544b76ad00d517040a880
scKamannavar <sagar080897@gmail.com>
2022-06-29 17:01:19 +1300

    firstChild

--- task7/dom.js
+++ task7/dom.js
@@ -1,5 +1,8 @@
 let x = document.querySelector('#items');
-console.log(x.parentElement);
+// console.log(x.parentElement);
 x.parentElement.style.backgroundColor = 'lightGrey';
 
 x.lastElementChild.style.backgroundColor =  'lightBlue';
+
+x.firstChild.style.backgroundColor =  'lightBlue';
+// console.log(x.firstChild);
