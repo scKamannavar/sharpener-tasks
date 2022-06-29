7a4a46cc186710a3cbdbe85b7e5fc78c92d69bb4
scKamannavar <sagar080897@gmail.com>
2022-06-29 15:48:31 +1300

    queryselector tasks

---
+++ task6/dom.js
@@ -0,0 +1,6 @@
+let x2 =  document.querySelector('.list-group-item:nth-child(2)');
+x2.style.color = 'green';
+
+let x3 =  document.querySelector('.list-group-item:nth-child(3)');
+x3.style.color = 'white';
+// x3.style.display = 'none';
