e583e2bcb668a558ba73b2fd2c2a8a6434c5a64f
scKamannavar <sagarck08@gmail.com>
2023-01-22 17:22:34 +0530

    Dynamic Routes Part 1

--- Express/productapp/app.js
+++ Express/productapp/app.js
@@ -3,9 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-
 const errorController = require('./controllers/error');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
